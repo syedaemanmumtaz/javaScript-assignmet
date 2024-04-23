2cf7f866f8fb16bae39989f5a13811207bfce8bb
syedaemanmumtaz <158057266+syedaemanmumtaz@users.noreply.github.com>
2024-04-23 22:52:41 +0500

    assign commit

---
+++ assignment 1 ALERT/app.js
@@ -0,0 +1,22 @@
+// ALERT
+
+// alert ("hello world");
+// console.log(alert);
+
+//qs 01
+// alert("Hello.... Welcome");
+
+//qs 02
+// alert("Error!Please enter a valid password");
+
+//qs 03
+// alert("Welcome to js land... \n happy coding");
+
+//qs 04
+// alert("Welcome to js land");
+// alert("prevent this page from creating additional dialogs")
+
+
+//qs 05
+// alert("helio world...hello world");
+
