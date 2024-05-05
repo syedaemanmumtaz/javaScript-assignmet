98e99c61136e2979236bfb00c9e615f59c1697f2
syedaemanmumtaz <158057266+syedaemanmumtaz@users.noreply.github.com>
2024-05-05 20:21:25 +0500

    js commit

---
+++ Assignment 2  Chap 2-5/app.js
@@ -0,0 +1,333 @@
+                                //variables for strings
+
+
+// var message = ("Assalamualikum this is my first JS assignment");
+// alert (message);
+// console.log(message);
+
+// var userName;
+// var firstName="Jhon";
+// alert(firstName);
+
+
+// let message = "hello world";
+// alert(message);
+
+// let one = "PIZZA \n PIZZ \n PIZ \n PI";
+// alert(one);
+
+// let child = 2;
+// let partner ="  XYZ";
+// let place ="  canada";
+// let job ="  data scientist";
+// alert(child + partner + place + job)
+
+// let email ="abc";
+// alert("my e-mail is"+" " +email);
+
+// let book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn js from the book"+" "+book);
+
+
+                              //variables for numbers
+
+
+// let age = 14;
+
+// console.log ("I am" ,age ," years old");
+
+
+// let number = 21;
+
+// console.log("you have visited this site ",number," times");
+
+
+// let birth_year =1999;
+
+// document.write("My birth year is ", birth_year ,  " data type is number");
+
+// let Visitor_name ="Saima Khan";
+// let Product_title ="T-Shirt";
+// let Quantity ="4";
+// console.log(Visitor_name , " ordered" ,Quantity, Product_title);
+
+
+                               //VARIABLES LEGAL & ILLEGAL
+
+
+// let variable1 , variable2 , variable3 ;
+
+
+// let variable4 ;
+
+// let variable_5 ;
+// let variable$6;
+// let variableSeven;
+
+// let variable;
+
+//let VARIABLE ;
+//let 1varkc ;
+//let illegal-variable;
+//let Name;
+//let Class ;
+
+// console.log("A heading stating “Rules for naming JS variables”");
+// console.log("$my_1stVariable" );
+// console.log("$name, _name or name" );
+// console.log("sensitive" );
+// console.log("reserved keywords" );
+
+// qs 1
+// var int1 = 10 ;
+// var int2 =9 ;
+// var int3 = int1 + int2 ;
+// console.log("sum of 10 + 9 =", int3);
+
+//Q 2
+// var int1 = 15 ;
+// var int2 =9 ;
+// var int3 = int1 -  int2 ;
+// console.log("sum of 15 -  9 =", int3);
+
+// var int1 = 15 ;
+// var int2 =9 ;
+// var  int3 = int1 * int2 ;
+// console.log("sum of 15 * 9 =", int3);
+
+// var int1 = 15 ;
+// var int2 =9 ;
+// var int3 = int1 /  int2 ;
+// console.log("sum of 15 /  9 =", int3);
+
+// var int1 = 15 ;
+// var int2 =9 ;
+// var int3 = int1 %  int2 ;
+// console.log("sum of 15 %  9 =", int3);
+
+//q 3
+// var a ;
+// console.log("Value after variable declaration is:" ,a);
+// a = 3 ;
+// console.log("“Initial value:", a);
+// ++a;
+// console.log("“Value after increment is:",a);
+// a= a + 7 ;
+// console.log("“Value after addition is:",a);
+// a--;
+// console.log("“Value after decrement is:" , a);
+// a = 10 % 3;
+// console.log("“The remainder is:" ,a );
+
+//qs no 4
+// var b = 600;
+// b = b * 5 ;
+// console.log("The cost to buy 5 tickets to a movie is", b , " PKR");
+
+//qs 6
+// var c = 53;
+// var f = (c * 9/5) + 32; 
+// console.log(c + "°C is " + f + "°F");
+
+// var f = 5 ;
+// var c = (f - 32)*5/9;
+// console.log(f + "°F is " + c +"°C");
+
+//qs 7 
+// console.log("SHOPPING CART ");
+// var poi1 = 560;
+// console.log ("price of item 1:", poi1);
+// var qoi1=2;
+// console.log("Ordered quantity of item 1:", qoi1);
+// var poi2 = 200;
+// var poi1 = 560;
+// console.log("price of item 2:", poi2);
+// var qoi2=5;
+// console.log("Ordered quantity of item 2:", qoi2);
+// var s_p = 150 ;
+// console.log ("Shipping Charges: ", s_p);
+// var tc = poi1*qoi1 + poi2*qoi2 + s_p;
+// console.log("Total price:",tc);
+
+
+//qs 8
+// var tm =550;
+// console.log("total marks =", tm);
+// var om=510;
+// console.log("obtained marks =" ,om);
+// var percentage = om/tm*100;
+// console.log("percentage =" ,percentage);
+
+//qs 9
+// var $ = 104 ;
+// var s_r = 28 ;
+// var pkr = 104*10 + 28*25;
+// console.log("Total curency in pakistani rupees:",pkr ,"PKR");
+
+//qs 10
+// var num = 3;
+// num = num + 5;
+// num = num * 10;
+// num = num / 2;
+// console.log(num);
+
+
+
+                                // MATH EXPRESSION
+
+// Q1
+// var number1 = prompt("Enter the first number:");
+// var number2 = prompt("Enter the second number:");
+// var result = parseFloat(number1) + parseFloat(number2);
+// document.write("<h1>Result:</h1>");
+// document.write("<p>" + number1 + " + " + number2 + " = " + result + "</p>");
+
+
+// qs 2
+// console.log("Multipication of 5")
+// var a = 5 ;
+// a*1;
+// console.log(a);
+// a=a*2;
+// console.log(a);
+// a=5*3;
+// console.log(a);
+// a=5*4;
+// console.log(a);
+// a=5*5;
+// console.log(a);
+// a=5*6;
+// console.log(a);
+// a=5*7;
+// console.log(a);
+// a=5*8;
+// console.log(a);
+// a=5*9;
+// console.log(a);
+// a=5*10;
+// console.log(a);
+
+// Q2
+
+// var number1 = prompt("Enter the first number:");
+// var number2 = prompt("Enter the second number:");
+// var result = parseFloat(number1) - parseFloat(number2);
+// document.write("<h1>Result:</h1>");
+// document.write("<p>" + number1 + " - " + number2 + " = " + result + "</p>");
+// var number1 = prompt("Enter the first number:");
+// var number2 = prompt("Enter the second number:");
+// var result = parseFloat(number1) * parseFloat(number2);
+// document.write("<h1>Result:</h1>");
+// document.write("<p>" + number1 + " * " + number2 + " = " + result + "</p>");
+// var number1 = prompt("Enter the first number:");
+// var number2 = prompt("Enter the second number:");
+// var result = parseFloat(number1) / parseFloat(number2);
+// document.write("<h1>Result:</h1>")
+// document.write("<p>" + number1 + " / " + number2 + " = " + result + "</p>");
+// var number1 = prompt("Enter the first number:");
+// var number2 = prompt("Enter the second number:");
+// var result = parseFloat(number1) % parseFloat(number2);
+// document.write("<h1>Result:</h1>");
+// document.write("<p>" + number1 + " % " + number2 + " = " + result + "</p>");
+
+// Q3(a)
+// var myVariable =50;
+// console.log(myVariable);
+
+// Q3(b)
+// var myVariable;
+// console.log ("Value after variable declaration is:", myVariable);
+
+// Q3(C)
+// var x = 10; 
+// var remainder = x % 3;
+// console.log("The remainder is: " + remainder);
+
+// Q3(d)
+// var x = 3;
+// console.log("Initial value: " + x);
+
+// Q3(E)
+// var x = 3; 
+// x++;
+// console.log("Value after increment is: " + x);
+
+// Q3(f)
+// var x = 11;
+// x--;
+// console.log("value after decrement is:" + x);
+
+// Q4
+// var ticketPrice = 600;
+// var numberOfTickets = 5;
+// var totalCost = ticketPrice * numberOfTickets;
+// console.log("Cost of one movie ticket is: " + ticketPrice + " PKR");
+// console.log("Cost of buying " + numberOfTickets + " tickets is: " + totalCost + " PKR");
+
+// Q5
+// var multiplicand = 8; 
+// document.write("<h2> Multiplication Table for " + multiplicand + "</h2>");
+// for (let i = 1; i <= 10; i++) {
+// document.write(multiplicand + " x " + i + " = " + (multiplicand * i) + "<br>");}
+
+// Q6
+// var celsiusTemperature = 35°C;
+// console.log(celsiusTemperature);
+
+// Q7
+// var celsiusTemperature = 25;
+// var fahrenheitTemperature = celsiusTemperature * 9/5 + 32 ;
+// console.log (celsiusTemperature + "°C is " + fahrenheitTemperature + "°F");
+
+// Q8
+// var fahrenheitTemperature = 77;
+// var celsiusTemperature = (fahrenheitTemperature - 32) * 5/9 ;
+// console.log(fahrenheitTemperature + "°F is " + celsiusTemperature.toFixed(2) + "°C");
+
+// var fahrenheitTemperature = 77; 
+// var celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+// console.log(fahrenheitTemperature + "°F is " + celsiusTemperature + "°C");
+
+// // Q9
+// var priceItem1 = 20.99;
+// var priceItem2 = 15.49;
+// var quantityItem1 = 2;
+// var quantityItem2 = 3;
+// var shippingCharges = 5.99;
+// var totalCostItem1 = priceItem1 * quantityItem1;
+// var totalCostItem2 = priceItem2 * quantityItem2;
+// var subtotal = totalCostItem1 + totalCostItem2;
+// var totalCostWithShipping = subtotal + shippingCharges;
+// document.write(" <h1>  SHOPING CARD </h1>"+ "<br>" +"<br>")
+// document.write("Price of Item 1 : " + priceItem1 + "<br>");
+// document.write("Quantity of Item 1 : " + quantityItem1 +"<br>");
+// document.write("Total Cost of Item 1 : " + totalCostItem1.toFixed(2) +"<br>");
+// document.write ("\nPrice of Item 2 : " + priceItem2 +"<br>");
+// document.write ("Quantity of Item 2 : " + quantityItem2 +"<br>");
+// document.write("Total Cost of Item 2 : " + totalCostItem2.toFixed(2) +"<br>");
+// document.write ( "\nSubtotal : " + subtotal.toFixed(2) + "<br>"+"<br>"+"<h4>");
+// document.write ("Shipping Charges : " + shippingCharges.toFixed(2) +"<br>"+"<br>" +"<h4>");
+// document.write("\nTotal Cost with Shipping : " + totalCostWithShipping.toFixed(2));
+
+// // Q#...
+// var  favoriteSnack = "chocolate bars";
+// var  currentAge = 30;
+// var  maximumAge = 80;
+// var  estimatedAmountPerDay = 1; 
+// var  yearsRemaining = maximumAge - currentAge;
+// var  daysRemaining = yearsRemaining * 365;
+// var  totalNeeded = daysRemaining * estimatedAmountPerDay;
+// document.write(`<h1>The Lifetime Supply Calculater</h1><br><br>`);
+// document.write(`Favorite Snack :${favoriteSnack}<br>`)
+// document.write(`Current Age :${currentAge}<br>`)
+// document.write(`Estimated Maximum Age :${maximumAge}<br>`)
+// document.write(`Amount of snacks per day :${estimatedAmountPerDay}<br>`)
+// document.write(`You will need ${totalNeeded} ${favoriteSnack} to last you until the ripe old age of ${maximumAge}.`);
+
+
+// Q#0..
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+//          //  1 - 0  + 1  +  1 = 3
+// document.write(`a: ${a} , b: ${b} <br> Result :${result}`);
